dca9e0f35b7a5de4c84078d7f9e116d3908b87c9
Devan Hedglin <dlhedglin@mail.csuchico.edu>
2020-11-16 11:48:37 -0800

    added tests

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -18,5 +18,50 @@ TEST(PracticeTest, is_simple_palindrome)
 {
     Practice obj;
     bool actual = obj.isPalindrome("Not a palindrome");
+    ASSERT_FALSE(actual);
+}
+TEST(PracticeTest, is_not_simple_palindrome)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("racecar");
     ASSERT_TRUE(actual);
 }
+TEST(PracticeTest, empty_pal)
+{
+    Practice obj;
+    bool actual = obj.isPalindrome("");
+    ASSERT_TRUE(actual);
+}
+TEST(PracticeTest, sort_from_ascending)
+{
+    Practice obj;
+	int first, second, third;
+	first = 1;
+	second = 2;
+	third = 3;
+    obj.sortDescending(first, second, third);
+	ASSERT_TRUE(first > second && second > third && first > third);
+
+}
+TEST(PracticeTest, sort_duplicate_numbers)
+{
+    Practice obj;
+	int first, second, third;
+	first = 3;
+	second = 3;
+	third = 3;
+    obj.sortDescending(first, second, third);
+    ASSERT_FALSE(first > second && second > third && first > third);
+}
+TEST(PracticeTest, sort_from_sorted)
+{
+    Practice obj;
+	int first, second, third;
+	first = 3;
+	second = 2;
+	third = 1;
+    obj.sortDescending(first, second, third);
+	ASSERT_TRUE(first > second && second > third && first > third);
+
+}
+
